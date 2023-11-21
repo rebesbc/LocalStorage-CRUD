000cb22e29309994ec52f686b8fa4e2b8342cc92
rebesbc <19170549@itculiacan.edu.mx>
2023-11-21 00:03:51 -0700

    feat: Show all books

---
+++ script.js
@@ -0,0 +1,56 @@
+function validateForm(){
+    var title = document.getElementById("title").value;
+    var author = document.getElementById("author").value;
+    var publisher = document.getElementById("publisher").value;
+    var isbn = document.getElementById("isbn").value;
+    // var release_date = document.getElementById("release_date").value;
+
+    if(title == ""){
+        alert("Title is required");
+        return false;
+    }
+
+    if(author == ""){
+        alert("Author is required");
+        return false;
+    }
+
+    if(publisher == ""){
+        alert("Publisher is required");
+        return false;
+    }
+
+    if(isbn == ""){
+        alert("ISBN is required");
+        return false;
+    }
+
+    return true;
+}
+
+function showBooks(){
+    var booksList;
+    if(localStorage.getItem("booksList") == null){
+        booksList = [];
+    }
+    else{
+        booksList = JSON.parse(localStorage.getItem("booksList"))
+    }
+
+    var html = "";
+    booksList.forEach(function (element, index){
+        html += "<tr>";
+        html += "<td>" + index + "</td>";
+        html += "<td>" + element.title + "</td>";
+        html += "<td>" + element.author + "</td>";
+        html += "<td>" + element.publisher + "</td>";
+        html += "<td>" + element.isbn + "</td>";
+        html += "<td>" + element.release_date + "</td>";
+        html += '<td><button onclick="deleteBook(' + index + ')" class="btn btn-danger">Delete</button><button onclick="updateBook(' + index + ')" class="btn btn-warning m-2">Edit</button></td>';
+        html += "</tr>"
+    });
+
+    document.querySelector("#crudTable tbody").innerHTML = html;
+}
+
+document.onload = showBooks();
